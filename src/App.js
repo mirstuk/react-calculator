416fea7a433e325d43a85bb3d9efcdf72e288d4d
Francesco Mirabella <mirstuk@users.noreply.github.com>
2020-07-16 10:04:00 +0200

    app layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">
+        <div className="formula">150+270</div>
+        <div className="result">420</div>
+      </div>
+      <div className="keyboard">
+        <div className="key gray">CE</div>
+        <div className="key gray">C</div>
+        <div className="key gray">±</div>
+        <div className="key gray">÷</div>
+        <div className="key">7</div>
+        <div className="key">8</div>
+        <div className="key">9</div>
+        <div className="key gray">×</div>
+        <div className="key">4</div>
+        <div className="key">5</div>
+        <div className="key">6</div>
+        <div className="key gray">-</div>
+        <div className="key">1</div>
+        <div className="key">2</div>
+        <div className="key">3</div>
+        <div className="key gray">+</div>
+        <div className="key void"></div>
+        <div className="key">0</div>
+        <div className="key">.</div>
+        <div className="key red">=</div>
+      </div>
     </div>
   );
 }
