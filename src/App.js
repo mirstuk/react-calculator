440a2190f4bbacc767f020948b355db0b18c97ad
Francesco Mirabella <mirstuk@users.noreply.github.com>
2020-08-11 09:29:34 +0200

    id on html key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
+function handleKey(e) {
+  const key = e.key.toUpperCase();
+  const keys = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '/',
+    '*',
+    '-',
+    '+',
+    '.',
+    'DELETE',
+    'BACKSPACE',
+    'ENTER',
+  ];
+  if (keys.includes(key)) {
+    document.getElementById(key).classList.add('active');
+    setTimeout(
+      () => document.getElementById(key).classList.remove('active'),
+      300
+    );
+  } else {
+    console.log(key);
+  }
+}
+
+const App = () => {
+  useEffect(() => {
+    document.addEventListener('keydown', handleKey);
+    return () => {
+      document.removeEventListener('keydown', handleKey);
+    };
+  }, []);
+
   return (
     <div className="calculator">
       <div className="display">
@@ -9,29 +49,67 @@ function App() {
         <div className="result">420</div>
       </div>
       <div className="keyboard">
-        <div className="key gray">CE</div>
-        <div className="key gray">C</div>
-        <div className="key gray">±</div>
-        <div className="key gray">÷</div>
-        <div className="key">7</div>
-        <div className="key">8</div>
-        <div className="key">9</div>
-        <div className="key gray">×</div>
-        <div className="key">4</div>
-        <div className="key">5</div>
-        <div className="key">6</div>
-        <div className="key gray">-</div>
-        <div className="key">1</div>
-        <div className="key">2</div>
-        <div className="key">3</div>
-        <div className="key gray">+</div>
+        <div className="key gray" id="DELETE">
+          CE
+        </div>
+        <div className="key gray" id="BACKSPACE">
+          C
+        </div>
+        <div className="key gray" id="">
+          ±
+        </div>
+        <div className="key gray" id="/">
+          ÷
+        </div>
+        <div className="key" id="7">
+          7
+        </div>
+        <div className="key" id="8">
+          8
+        </div>
+        <div className="key" id="9">
+          9
+        </div>
+        <div className="key gray" id="*">
+          ×
+        </div>
+        <div className="key" id="4">
+          4
+        </div>
+        <div className="key" id="5">
+          5
+        </div>
+        <div className="key" id="6">
+          6
+        </div>
+        <div className="key gray" id="-">
+          -
+        </div>
+        <div className="key" id="1">
+          1
+        </div>
+        <div className="key" id="2">
+          2
+        </div>
+        <div className="key" id="3">
+          3
+        </div>
+        <div className="key gray" id="+">
+          +
+        </div>
         <div className="key void"></div>
-        <div className="key">0</div>
-        <div className="key">.</div>
-        <div className="key red">=</div>
+        <div className="key" id="0">
+          0
+        </div>
+        <div className="key" id=".">
+          .
+        </div>
+        <div className="key red" id="ENTER">
+          =
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
